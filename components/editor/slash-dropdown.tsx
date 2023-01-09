2563100f54aa57f20e449b3ea92ed75633dc96f6
Vu Van Dung <me@joulev.dev>
2023-01-10 00:12:19 +0800

    filter buttons according to query
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor/slash-dropdown.tsx
+++ components/editor/slash-dropdown.tsx
@@ -18,22 +18,29 @@ export default function SlashDropdown({
 }) {
   const editor = useSlate();
   const plateEditor = usePlateEditorRef();
+  const buttonsToShow = (
+    [
+      [ELEMENT_H1, "Heading 1", "h1"],
+      [ELEMENT_H2, "Heading 2", "h2"],
+      [ELEMENT_PARAGRAPH, "Paragraph", "p"],
+    ] as const
+  ).filter(el => el[2].includes(query));
   return (
     <div
-      className="fixed z-10 flex flex-col rounded border border-slate-200 bg-white p-3 transition-all"
+      className="fixed z-10 flex w-36 flex-col rounded border border-slate-200 bg-white p-3 transition-all"
       style={{ top, left }}
     >
-      {[
-        [ELEMENT_H1, "Heading 1"],
-        [ELEMENT_H2, "Heading 2"],
-        [ELEMENT_PARAGRAPH, "Paragraph"],
-      ].map(([type, label], index) => (
+      {buttonsToShow.length === 0 && <div className="text-gray-400">No results</div>}
+      {buttonsToShow.map(([type, label], index) => (
         <button
           key={index}
           onClick={() =>
-            editor.insertNode({ type: getPluginType(plateEditor, type), children: [{ text: "" }] })
+            editor.insertNode({
+              type: getPluginType(plateEditor, type),
+              children: [{ text: "" }],
+            })
           }
-          className="rounded px-2 py-1 transition hover:bg-slate-200"
+          className="rounded px-2 py-1 text-left transition hover:bg-slate-200"
         >
           {label}
         </button>
