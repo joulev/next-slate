a79d6502dcb7fd7e0bee334bd8667fe5665fdd4f
Vu Van Dung <me@joulev.dev>
2023-01-09 21:24:33 +0800

    add basic mark plugins and get ready for custom styling
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,9 +1,37 @@
-import { Plate, TEditableProps } from "@udecode/plate";
+import {
+  Plate,
+  TEditableProps,
+  createPlugins,
+  createBlockquotePlugin,
+  createCodeBlockPlugin,
+  createHeadingPlugin,
+  createParagraphPlugin,
+  createBasicMarksPlugin,
+} from "@udecode/plate";
+import { initialValue } from "~/components/editor/initial-value";
+import { plateUI } from "~/components/editor/plate-ui";
+
+const plugins = createPlugins(
+  [
+    createBlockquotePlugin(),
+    createCodeBlockPlugin(),
+    createHeadingPlugin(),
+    createParagraphPlugin(),
+    createBasicMarksPlugin(),
+  ],
+  { components: plateUI }
+);
 
 const editableProps: TEditableProps = {
   placeholder: "Type something...",
 };
 
 export default function Editor({ className }: { className?: string }) {
-  return <Plate editableProps={{ ...editableProps, className }} />;
+  return (
+    <Plate
+      editableProps={{ ...editableProps, className }}
+      plugins={plugins}
+      initialValue={initialValue}
+    />
+  );
 }
