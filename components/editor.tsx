97853bcebb0afafb50fa1aece4dec1adb0ff1a94
Vu Van Dung <me@joulev.dev>
2023-01-09 22:40:34 +0800

    set up exit break plugin because that's what I expect in Notion
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor.tsx
+++ components/editor.tsx
@@ -5,12 +5,14 @@ import {
   createBasicElementsPlugin,
   createBasicMarksPlugin,
   createNodeIdPlugin,
+  createExitBreakPlugin,
 } from "@udecode/plate";
 import { createDndPlugin } from "@udecode/plate-ui-dnd";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { initialValue } from "~/components/editor/initial-value";
 import { components } from "~/components/editor/components";
+import { exitBreakPlugin } from "~/components/editor/exit-break";
 
 const plugins = createPlugins(
   [
@@ -18,6 +20,7 @@ const plugins = createPlugins(
     createBasicMarksPlugin(),
     createNodeIdPlugin(),
     createDndPlugin({ options: { enableScroller: true } }),
+    createExitBreakPlugin(exitBreakPlugin),
   ],
   { components }
 );
