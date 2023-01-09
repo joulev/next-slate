df046807ccb2f8eaa22440ff73763f63ccd3921f
Vu Van Dung <me@joulev.dev>
2023-01-09 20:54:51 +0800

    use plate; slate ssr workaround is no longer needed
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import { createEditor, Descendant } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
-import { withHistory } from "slate-history";
+import { Plate, TEditableProps } from "@udecode/plate";
 
-const initialValue: Descendant[] = [
-  { children: [{ text: "This is editable plain text, just like a <textarea>!" }] },
-];
+const editableProps: TEditableProps = {
+  placeholder: "Type something...",
+};
 
 export default function Editor({ className }: { className?: string }) {
-  const [editor] = useState(() => withReact(withHistory(createEditor())));
-  return (
-    <Slate editor={editor} value={initialValue}>
-      <Editable placeholder="Enter some plain text..." className={className} />
-    </Slate>
-  );
+  return <Plate editableProps={{ ...editableProps, className }} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
-import dynamic from "next/dynamic";
-
-// Fix "Prop `data-slate-editor-id` did not match" error
-const Editor = dynamic(() => import("~/components/editor"), { ssr: false });
+import Editor from "~/components/editor";
 
 export default function IndexPage() {
   return <Editor className="mx-auto h-screen max-w-prose py-24 px-6" />;
