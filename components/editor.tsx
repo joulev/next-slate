0f5474d0f310bcaa187589c7f96fed155eae8ea9
Vu Van Dung <me@joulev.dev>
2023-01-09 23:00:09 +0800

    add a state controlling the value of the slash command
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,6 +1,5 @@
 import {
   Plate,
-  TEditableProps,
   createPlugins,
   createBasicElementsPlugin,
   createBasicMarksPlugin,
@@ -13,6 +12,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { initialValue } from "~/components/editor/initial-value";
 import { components } from "~/components/editor/components";
 import { exitBreakPlugin } from "~/components/editor/exit-break";
+import { useState } from "react";
 
 const plugins = createPlugins(
   [
@@ -25,15 +25,32 @@ const plugins = createPlugins(
   { components }
 );
 
-const editableProps: TEditableProps = {
-  placeholder: "Type something...",
-};
-
 export default function Editor({ className }: { className?: string }) {
+  const [slashQuery, setSlashQuery] = useState<string | null>(null);
   return (
     <DndProvider backend={HTML5Backend}>
+      {slashQuery === null ? <div>null</div> : <div>({slashQuery})</div>}
       <Plate
-        editableProps={{ ...editableProps, className }}
+        editableProps={{
+          className,
+          onKeyDown: e => {
+            switch (e.key) {
+              case "/":
+                setSlashQuery("");
+                break;
+              case "Enter":
+              case " ":
+                setSlashQuery(null);
+                break;
+              case "Backspace":
+                if (slashQuery && slashQuery.length > 0) setSlashQuery(slashQuery.slice(0, -1));
+                else setSlashQuery(null);
+                break;
+              default:
+                if (e.key.length === 1) setSlashQuery(slashQuery + e.key);
+            }
+          },
+        }}
         plugins={plugins}
         initialValue={initialValue}
       />
