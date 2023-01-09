bc3f41e5676e98599999d6ca9cbd90cd52187548
Vu Van Dung <me@joulev.dev>
2023-01-09 22:03:49 +0800

    improve styling of the drag-n-drop icon
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- components/editor/plate-ui.tsx
+++ components/editor/plate-ui.tsx
@@ -3,52 +3,34 @@ import { withDraggables } from "@udecode/plate-ui-dnd";
 
 export const plateUI = withDraggables(
   createPlateUI({
-    [ELEMENT_H1]: props => <h1 {...props} className="mt-6 mb-3 text-2xl font-bold sm:text-3xl" />,
-    [ELEMENT_H2]: props => <h2 {...props} className="mt-6 mb-3 text-xl font-bold sm:text-2xl" />,
-    [ELEMENT_PARAGRAPH]: props => <p {...props} className="mb-3" />,
+    [ELEMENT_H1]: props => <h1 {...props} className="mt-8 mb-4 text-2xl font-bold sm:text-3xl" />,
+    [ELEMENT_H2]: props => <h2 {...props} className="mt-8 mb-4 text-xl font-bold sm:text-2xl" />,
+    [ELEMENT_PARAGRAPH]: props => <p {...props} className="mb-4" />,
     // and so on, but let's not style all elements as it's not the main scope. Also other than the above,
     // the default styling by Plate is nice enough.
   }),
   [
-    {
-      keys: [ELEMENT_PARAGRAPH],
-      level: 0,
-    },
+    { keys: [ELEMENT_PARAGRAPH], level: 0 },
     {
       keys: [ELEMENT_PARAGRAPH, ELEMENT_H1, ELEMENT_H2],
-      onRenderDragHandle: () => <button type="button">D</button>,
+      onRenderDragHandle: () => (
+        <button type="button" className="text-slate-500 transition hover:text-slate-900">
+          {/** @source Lucide Icon */}
+          <svg
+            className="h-4 w-4 fill-none stroke-current stroke-2 [stroke-linecap:round] [stroke-linejoin:round]"
+            viewBox="0 0 24 24"
+          >
+            <circle cx="9" cy="12" r="1"></circle>
+            <circle cx="9" cy="5" r="1"></circle>
+            <circle cx="9" cy="19" r="1"></circle>
+            <circle cx="15" cy="12" r="1"></circle>
+            <circle cx="15" cy="5" r="1"></circle>
+            <circle cx="15" cy="19" r="1"></circle>
+          </svg>
+        </button>
+      ),
     },
-    {
-      key: ELEMENT_H1,
-      styles: {
-        gutterLeft: {
-          padding: "2em 0 4px",
-          fontSize: "1.875em",
-        },
-        blockToolbarWrapper: {
-          height: "1.3em",
-        },
-      },
-    },
-    {
-      key: ELEMENT_H2,
-      styles: {
-        gutterLeft: {
-          padding: "1.4em 0 1px",
-          fontSize: "1.5em",
-        },
-        blockToolbarWrapper: {
-          height: "1.3em",
-        },
-      },
-    },
-    {
-      keys: [ELEMENT_PARAGRAPH],
-      styles: {
-        gutterLeft: {
-          padding: "4px 0 0",
-        },
-      },
-    },
+    { key: ELEMENT_H1, styles: { gutterLeft: { marginTop: "2.5rem" } } },
+    { key: ELEMENT_H2, styles: { gutterLeft: { marginTop: "2.5rem" } } },
   ]
 );
